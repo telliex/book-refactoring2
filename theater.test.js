40fecf20a4afc0219cec53459f5f7b9cf68a251b
Telliex <telliexyuzo@gmail.com>
2019-06-27 16:50:40 +0800

    first origi code

---
+++ theater.test.js
@@ -0,0 +1,20 @@
+import invoice from './invoice.json';
+import plays from './plays.json';
+import statement from './theater.js';
+
+describe('This is Theater',()=>{
+
+  describe('output match ',()=>{
+
+    test('put default data', () => {
+      expect(statement(invoice,plays)).toEqual(expect.stringContaining(`Statement for BigCo
+tragedy: $650.00 (55 seats)
+comedy: $580.00 (35 seats)
+tragedy: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits`));
+    })
+
+  })
+  
+})
